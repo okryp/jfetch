307745e83488c1472872d401ca490d6b81aff73e
jrasb <okrypczyk@protonmail.com>
2022-11-27 14:58:14 +0100

    changed uptime and replaced ; with :

--- jfetch.sh
+++ jfetch.sh
@@ -6,11 +6,11 @@ BROWSER=`xdg-settings get default-web-browser`;
 TIME=`date +%H:%M:%S`
 DATE=`date +%d/%m/%y`
 MEMORY=``;
-UPTIME=`uptime --since`;
+UPTIME=`uptime`;
 
 echo -e "\e[1;31mDISTRO:\e[0m $DISTRO";
 echo -e "\e[1;31mUSER:\e[0m $USER";
 echo -e "\e[1;31mBROWSER:\e[0m $BROWSER";
 echo -e "\e[1;31mTIME:\e[0m $TIME";
 echo -e "\e[1;31mDATE:\e[0m $DATE"; 
-echo -e "\e[1;31mUPTIME;\e[0m $UPTIME";
+echo -e "\e[1;31mUPTIME:\e[0m $UPTIME";

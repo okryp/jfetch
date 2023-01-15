2bd1fd18e5eda5875f734069849a764e63435865
jrasb <okrypczyk@protonmail.com>
2023-01-15 14:00:33 +0100

    Removed bashisms

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 # Makes a symbolic link in the bin folder so that pfetch is availible without accessing this
 # folder all the time.
 
-dir=`pwd`;
+DIR=$(pwd);
 
 echo "Installing jfetch in the /usr/bin/ directory";
 
-ln -s $dir/jfetch.sh /usr/bin/jfetch;
+ln -sfT "$DIR"/jfetch.sh /usr/bin/jfetch;
--- jfetch.sh
+++ jfetch.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-DISTRO=`cat /etc/system-release`
-USER=`whoami`;
-BROWSER=`xdg-settings get default-web-browser`;
-TIME=`date +%H:%M:%S`
-DATE=`date +%d/%m/%y`
-MEMORY=``;
-UPTIME=`uptime --pretty`;
+DISTRO=$(cat /etc/system-release)
+USER=$(whoami);
+BROWSER=$(xdg-settings get default-web-browser);
+TIME=$(date +%H:%M:%S)
+DATE=$(date +%d/%m/%y)
+UPTIME=$(uptime --pretty);
 
-echo -e "\e[1;31mDISTRO:\e[0m $DISTRO";
-echo -e "\e[1;31mUSER:\e[0m $USER";
-echo -e "\e[1;31mBROWSER:\e[0m $BROWSER";
-echo -e "\e[1;31mTIME:\e[0m $TIME";
-echo -e "\e[1;31mDATE:\e[0m $DATE"; 
-echo -e "\e[1;31mUPTIME:\e[0m $UPTIME";
+BOLD=$(tput bold)
+LIGHT_RED=$(tput setaf 001)
+NORMAL_TEXT=$(tput sgr0)
+
+printf '%s\n' "DISTRO: ${LIGHT_RED}${BOLD}${DISTRO}${NORMAL_TEXT}";
+printf '%s\n' "USER: ${LIGHT_RED}${BOLD}${USER}${NORMAL_TEXT}";
+printf '%s\n' "BROWSER: ${LIGHT_RED}${BOLD}${BROWSER}${NORMAL_TEXT}";
+printf '%s\n' "TIME: ${LIGHT_RED}${BOLD}${TIME}${NORMAL_TEXT}";
+printf '%s\n' "DATE: ${LIGHT_RED}${BOLD}${DATE}${NORMAL_TEXT}";
+printf '%s\n' "UPTIME: ${LIGHT_RED}${BOLD}${UPTIME}${NORMAL_TEXT}";

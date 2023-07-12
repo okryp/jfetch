445be37805719d176a16313ceb27e9024c4afc20
okryp <okrypczyk@protonmail.com>
2023-07-12 14:21:18 +0200

    reworked jfetch.sh

--- jfetch.sh
+++ jfetch.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-DISTRO=$(cat /etc/system-release)
-USER=$(whoami);
-BROWSER=$(xdg-settings get default-web-browser);
+# Fetch vars
+OS=$(uname -o)
+USER=$(whoami)
 TIME=$(date +%H:%M:%S)
 DATE=$(date +%d/%m/%y)
-UPTIME=$(uptime --pretty);
+UPTIME=$(uptime --pretty)
 
+# Formatting related
 BOLD=$(tput bold)
 LIGHT_RED=$(tput setaf 001)
 NORMAL_TEXT=$(tput sgr0)
 
-printf '%s\n' "DISTRO: ${LIGHT_RED}${BOLD}${DISTRO}${NORMAL_TEXT}";
-printf '%s\n' "USER: ${LIGHT_RED}${BOLD}${USER}${NORMAL_TEXT}";
-printf '%s\n' "BROWSER: ${LIGHT_RED}${BOLD}${BROWSER}${NORMAL_TEXT}";
-printf '%s\n' "TIME: ${LIGHT_RED}${BOLD}${TIME}${NORMAL_TEXT}";
-printf '%s\n' "DATE: ${LIGHT_RED}${BOLD}${DATE}${NORMAL_TEXT}";
-printf '%s\n' "UPTIME: ${LIGHT_RED}${BOLD}${UPTIME}${NORMAL_TEXT}";
+printf "%s\t%s\n" "OS:" "$LIGHT_RED $BOLD $OS $NORMAL_TEXT"
+printf "%s\t%s\n" "USER:" "$LIGHT_RED $BOLD $USER $NORMAL_TEXT"
+printf "%s\t%s\n" "TIME:" "$LIGHT_RED $BOLD $TIME $NORMAL_TEXT"
+printf "%s\t%s\n" "DATE:" "$LIGHT_RED $BOLD $DATE $NORMAL_TEXT"
+printf "%s\t%s\n" "UPTIME:" "$LIGHT_RED $BOLD $UPTIME $NORMAL_TEXT"
